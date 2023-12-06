19824f720c19bf985c775d27a968eaa3d4d7aadf
dung.vt.vn <103554240+DungVt-VN@users.noreply.github.com>
2023-12-06 13:10:01 +0700

    Update Income.js

--- src/Components/Income/Income.js
+++ src/Components/Income/Income.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import styled from 'styled-components'
 import { useGlobalContext } from '../../context/globalContext';
 import { InnerLayout } from '../../styles/Layouts';
@@ -7,7 +7,11 @@ import IncomeItem from '../IncomeItem/IncomeItem';
 
 function Income() {
     const {addIncome,incomes, getIncomes, deleteIncome, totalIncome} = useGlobalContext()
+    const [titlex, setTitle] = useState(""); 
 
+    const handleChange = (event) => {
+        setTitle(event.target.value);
+    }
     useEffect(() =>{
         getIncomes()
     }, [])
@@ -21,9 +25,22 @@ function Income() {
                         <Form />
                     </div>
                     <div className="incomes">
+                        <div className="input-control">
+                            <input 
+                                type="text" 
+                                value={titlex}
+                                name={'title'} 
+                                placeholder="Nhap tu khoa de loc"
+                                onChange={handleChange}
+                            />
+                            <br />
+                            <br />
+                        </div>
                         {incomes.map((income) => {
                             const {_id, title, amount, date, category, description, type} = income;
-                            return <IncomeItem
+                            const check = title.concat(amount).concat(date).concat(category).concat(description);
+                            if(check.toLowerCase().search(titlex.trim().toLocaleLowerCase()) >= 0) {
+                                return <IncomeItem
                                 key={_id}
                                 id={_id} 
                                 title={title} 
@@ -35,6 +52,7 @@ function Income() {
                                 indicatorColor="var(--color-green)"
                                 deleteItem={deleteIncome}
                             />
+                            }
                         })}
                     </div>
                 </div>
@@ -70,6 +88,31 @@ const IncomeStyled = styled.div`
         .incomes{
             flex: 1;
         }
+    }
+
+
+    input, textarea, select{
+        font-family: inherit;
+        font-size: inherit;
+        outline: none;
+        border: none;
+        padding: .5rem 1rem;
+        border-radius: 5px;
+        border: 2px solid #fff;
+        background: transparent;
+        resize: none;
+        box-shadow: 0px 1px 15px rgba(0, 0, 0, 0.06);
+        color: rgba(34, 34, 96, 0.9);
+        &::placeholder{
+            color: rgba(34, 34, 96, 0.4);
+        }
+    }
+    .input-control{
+        input{
+            width: 100%;
+
+        }
+
     }
 `;
 
